238359e5ed89da7dbfedf6049c5963aa56dc0747
Abulkhair23 <78597371+Abulkhair23@users.noreply.github.com>
2021-04-28 17:25:50 +0600

    gg

--- backend/webdev/api/models.py
+++ backend/webdev/api/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name =  models.CharField(max_length=200)
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+    def to_json(self):
+        return {
+            'name' : self.name
+        }
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    name =  models.CharField(max_length=200)
+    description =  models.TextField()
+    price =  models.FloatField(default=0)
+    category =  models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+    def to_json(self):
+        return {
+            'name' : self.name,
+            'price' : self.price,
+            'category' : self.category
+        }
+
+    def __str__(self):
+        return f'{self.name}'
