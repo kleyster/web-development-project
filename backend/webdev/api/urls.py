857e5017e41fa31cd0e0b6d6a8305827d988a0ad
Abulkhair23 <78597371+Abulkhair23@users.noreply.github.com>
2021-04-29 17:51:51 +0600

    Update urls.py

---
+++ backend/webdev/api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    # path('', include('api.urls')),
+    # path('login/',),
+    # path('categories/<int:category_id>',)
+    # path('categories/<int:category_id>/<int:product_id>',)
+]
