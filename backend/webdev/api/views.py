3939ba4d61010c8d0e1182e1593e5a333c94b402
Renata Khaliyeva <khalievar@gmail.com>
2021-04-30 15:10:26 +0600

    gg

--- backend/webdev/api/views.py
+++ backend/webdev/api/views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
 
 from api.serializers import ProductSerializer
+from api.serializers import CategorySerializer2
 from api.models import Category, Product
+
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse, JsonResponse
+
+from rest_framework.request import Request
+from rest_framework.response import Response
+
 
 class ProductListAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -13,3 +22,18 @@ class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+
+
+@api_view(['GET', 'POST'])
+def category_list(request):
+    if request.method == 'GET':
+        categories = Category.objects.filter(name__contains='5').order_by('-id')
+        serializer = CategorySerializer2(categories, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CategorySerializer2(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
